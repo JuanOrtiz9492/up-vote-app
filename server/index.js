aa4d48e3a94d5e952aac1e3fc4ab1240704e7348
Juan Ortiz <ortizjuandavid@gmail.com>
2020-01-20 14:47:26 -0500

    db configured, its posible to get data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { Client } = require('pg')
+const DbVariables = require('./dataBaseConfig.json')
 
 app.use(express.static(path.join(__dirname, '../build')))
+
+app.get('/candidates', (req, res) => {
+    let { referencia } = req.query
+    const client = new Client(DbVariables)
+    client.connect()
+    client.query(`SELECT * FROM CONTESTANS ;`)
+        .then(result => res.json(result.rows))
+            .catch(e => console.log(e.stack))
+                .then( () => client.end() )
+}),
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'../build/index.html'))
